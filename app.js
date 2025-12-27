e2aadee5a8e1bba4292fcc805174be040e046e87
Sakhavat Fleydanov <sexavetfv@gmail.com>
2025-12-28 01:43:46 +0400

    the final codes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const flipSound = new Audio("sounds/shuffleEfect.mp3");
+const section = document.querySelector(".elements");
 const scoreTable = document.querySelector(".score_table");
 const pointsElement = document.querySelector("#pointCount");
 const lifeElement = document.querySelector("#lifeCount");
-const section = document.querySelector(".elements");
+const playAgain = document.querySelector("#play_again");
 let elements = ["javascript", "laravel", "mongo", "nodejs", "php", "react"];
 elements = [...elements, ...elements];
 let selected = [];
@@ -16,73 +17,88 @@ const updateScore = () => {
   lifeElement.textContent = `Life:${lifeCount}`;
 };
 updateScore();
-
 const flipCards = () => {
   document
     .querySelectorAll(".back")
     .forEach((a) => (a.style.transform = "rotateY(180deg)"));
 };
 setTimeout(flipCards, 2000);
-elements.forEach((a) => {
-  let img = document.createElement("img");
-  img.setAttribute("src", `images/${a}.png`);
-  let back_img = document.createElement("img");
-  back_img.setAttribute("src", "images/back.jpg");
-  let card = document.createElement("div");
-  card.classList.add("card");
-  card.addEventListener("click", () => {
-    if (!allow || card.classList.contains("active") || selected.length === 2) {
-      return;
-    }
-    flipSound.play();
-    card.classList.add("active");
-    selected.push(a);
-    if (selected.length === 2) {
-      if (selected[0] === selected[1]) {
-        pointCount++;
-        updateScore();
-        setTimeout(() => {
-          document
-            .querySelectorAll(".active")
-            .forEach((a) => a.classList.add("active2"));
-          // document
-          //   .querySelectorAll(".active")
-          //   .forEach((a) => a.classList.remove("active"));
-          selected = [];
-        }, 1000);
-      } else {
-        lifeCount--;
-        updateScore();
-        if (!lifeCount) {
-          document.querySelectorAll(".card:not(.active)").forEach((t) => {
-            t.classList.add(".active");
-          });
-          return;
-        }
-        setTimeout(() => {
-          flipSound.play();
-          document
-            .querySelectorAll(".active:not(.active2)")
-            .forEach((a) => a.classList.remove("active"));
-          selected = [];
-        }, 2000);
+const createGame = () => {
+  section.textContent = "";
+  pointCount = 0;
+  lifeCount = 3;
+  allow = true;
+  selected = [];
+  setTimeout(flipCards, 2000);
+
+  elements.forEach((a) => {
+    let img = document.createElement("img");
+    img.setAttribute("src", `images/${a}.png`);
+    let back_img = document.createElement("img");
+    back_img.setAttribute("src", "images/back.jpg");
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.addEventListener("click", () => {
+      if (
+        !allow ||
+        card.classList.contains("active") ||
+        selected.length === 2
+      ) {
+        return;
       }
-    }
-    console.log(selected);
+      flipSound.play();
+      card.classList.add("active");
+      selected.push(a);
+      if (selected.length === 2) {
+        if (selected[0] === selected[1]) {
+          pointCount++;
+          updateScore();
+          setTimeout(() => {
+            document
+              .querySelectorAll(".active")
+              .forEach((a) => a.classList.add("active2"));
+            // document
+            //   .querySelectorAll(".active")
+            //   .forEach((a) => a.classList.remove("active"));
+            selected = [];
+          }, 1000);
+        } else {
+          lifeCount--;
+          updateScore();
+          if (!lifeCount) {
+            document.querySelectorAll(".card:not(.active)").forEach((t) => {
+              t.classList.add("active");
+            });
+            playAgain.style.display = "block";
+            return;
+          }
+          setTimeout(() => {
+            flipSound.play();
+            document
+              .querySelectorAll(".active:not(.active2)")
+              .forEach((a) => a.classList.remove("active"));
+            selected = [];
+          }, 2000);
+        }
+      }
+      console.log(selected);
+    });
+    let card__inner = document.createElement("div");
+    card__inner.classList.add("card__inner");
+    let front = document.createElement("div");
+    front.classList.add("front");
+    let back = document.createElement("div");
+    back.classList.add("back");
+    back.append(img);
+    back.classList.add("back");
+    front.append(back_img);
+    card__inner.append(front, back);
+    card.append(card__inner);
+    section.append(card);
   });
-  let card__inner = document.createElement("div");
-  card__inner.classList.add("card__inner");
-  let front = document.createElement("div");
-  front.classList.add("front");
-  let back = document.createElement("div");
-  back.classList.add("back");
-  back.append(img);
-  back.classList.add("back");
-  front.append(back_img);
-  card__inner.append(front, back);
-  card.append(card__inner);
-  section.append(card);
-});
+};
+playAgain.addEventListener("click", createGame);
+createGame();
 
 document.addEventListener("transitionstart", () => {
   allow = false;
